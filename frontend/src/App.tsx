4e3be463c4cafcaacb3d27d182ccc3774a5b939f
Chr1ztt <chilllikehuman@gmail.com>
2025-03-02 20:30:04 +0700

    feat: add router functionality in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex p-2 gap-2  w-full bg-transparent">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <div
+          onClick={() => setCount(count + 1)}
+          className="select-none cursor-pointer p-2 rounded-md hover:bg-blue-300 w-fit m-2 hover:text-blue-950 bg-blue-700 text-white"
+        >
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        </div>
+      <Outlet />
+      <TanStackRouterDevtools />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/routes/__root.tsx
@@ -0,0 +1,9 @@
+import App from "@/App";
+import { createRootRoute } from "@tanstack/react-router";
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+
+export const Route = createRootRoute({
+  component: App
+})
+
