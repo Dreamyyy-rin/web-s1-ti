b641a688deb1dc74fc70c0a622d296da51b9be7a
Chr1ztt <chilllikehuman@gmail.com>
2025-05-04 12:10:01 +0700

    feat: add fetch announcement in index homepage

--- frontend/src/routes/_homeLayout/index.tsx
+++ frontend/src/routes/_homeLayout/index.tsx
@@ -20,17 +20,22 @@ import itexploreLink from "@/assets/itexploreLink.jpg";
 import {
   Card,
   CardContent,
-  CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import Footer from "@/components/ui/custom/footer/footer";
+import { useFetchAnnouncementsPaginated } from "@/features/announcement/hooks/useFetchAnnouncementsPaginated";
+import ReadonlyText from "@/components/ui/custom/rich-text-editor/readonlyText";
 
 export const Route = createFileRoute("/_homeLayout/")({
   component: Index,
 });
 
 function Index() {
+  const { data: announcements, isLoading: isFetchAnnouncementLoading } =
+    useFetchAnnouncementsPaginated({ per_page: 3, sort_by: "latest", page: 1 });
+
   return (
     <div>
       {/* Carousel */}
@@ -64,7 +69,7 @@ function Index() {
         </Carousel>
       </div>
 
-      <div className="container mx-auto flex flex-col md:flex-row justify-center items-center gap-4 mt-6 px-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-center items-center gap-4 mt-6 px-8">
         <Link
           to="/vacancy"
           className="flex justify-center items-center w-full md:w-1/3 h-12"
@@ -80,149 +85,112 @@ function Index() {
       </div>
 
       {/* Bagian Pengumuman */}
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-8 py-8">
         {/* Heading Pengumuman */}
         <div className="text-center mt-20">
           <h2 className="text-3xl font-semibold">Pengumuman</h2>
         </div>
 
         {/* Card Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
+        <div className="flex flex-wrap flex-initial justify-evenly  mt-6 -mx-2 ">
           {/* Card 1 */}
-          <Card className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <img
-              src={carousel1}
-              alt="Pengumuman 1"
-              className="w-full h-48 object-cover"
-            />
-            <CardContent>
-              <CardHeader>
-                <CardTitle className="text-xl font-semibold text-gray-900">
-                  Diskomvision 2024
-                </CardTitle>
-                <CardDescription className="text-gray-600 mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </CardDescription>
-              </CardHeader>
-              <Button className="mt-4" variant="outline" size="sm">
-                Read More
-              </Button>
-            </CardContent>
-          </Card>
-
-          {/* Card 2 */}
-          <Card className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <img
-              src={carousel1}
-              alt="Pengumuman 2"
-              className="w-full h-48 object-cover"
-            />
-            <CardContent>
-              <CardHeader>
-                <CardTitle className="text-xl font-semibold text-gray-900">
-                  Judul Pengumuman 2
-                </CardTitle>
-                <CardDescription className="text-gray-600 mt-2">
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </CardDescription>
-              </CardHeader>
-              <Button className="mt-4" variant="outline" size="sm">
-                Read More
-              </Button>
-            </CardContent>
-          </Card>
-
-          {/* Card 3 */}
-          <Card className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <img
-              src={carousel1}
-              alt="Pengumuman 3"
-              className="w-full h-48 object-cover"
-            />
-            <CardContent>
-              <CardHeader>
-                <CardTitle className="text-xl font-semibold text-gray-900">
-                  Judul Pengumuman 3
-                </CardTitle>
-                <CardDescription className="text-gray-600 mt-2">
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur.
-                </CardDescription>
-              </CardHeader>
-              <Button className="mt-4" variant="outline" size="sm">
-                Read More
-              </Button>
-            </CardContent>
-          </Card>
+          {announcements?.data.map((announcement) => (
+            <div className="w-full  md:w-1/2 xl:w-1/3 px-2 pb-4">
+              <Card
+                className=" bg-white rounded-lg shadow-lg overflow-hidden h-full flex flex-col "
+                key={announcement.id}
+              >
+                <img
+                  src={carousel1}
+                  alt="Pengumuman 1"
+                  className="w-full h-48 object-cover"
+                />
+                <CardHeader className="">
+                  <CardTitle className="text-xl font-semibold text-gray-900">
+                    {announcement.judul}
+                  </CardTitle>
+                  {/* <CardDescription className="text-gray-600 mt-2"></CardDescription> */}
+                </CardHeader>
+                <CardContent className="">
+                  <ReadonlyText maxlength={100} data={announcement.isi} />
+                </CardContent>
+                <CardFooter className="flex-auto items-end">
+                  <Button className="" variant="outline" size="sm">
+                    Read More
+                  </Button>
+                </CardFooter>
+              </Card>
+            </div>
+          ))}
         </div>
       </div>
 
       {/* Tombol Selengkapnya */}
-      <div className="container mx-auto flex justify-center mt-6 px-4">
+      <div className="container mx-auto flex justify-center px-8">
         <Link to="/announcement" className="w-full">
-          <Button variant="outline" className="w-full">Selengkapnya</Button>
+          <Button variant="outline" className="w-full">
+            Selengkapnya
+          </Button>
         </Link>
       </div>
 
       {/* Bagian Kerja Sama */}
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-8 py-8">
         <div className="text-center my-16">
           <h2 className="text-3xl font-semibold">Kerja Sama</h2>
         </div>
 
         <div className="container flex items-center justify-center flex-col md:flex-row mx-auto gap-28 px-4 mt-6 ">
           {/* <div className="justify-center gap-28 items-center "> */}
-            {/* Logo Alfamart */}
-            <a
-              href="https://www.alfamart.co.id/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="transition-transform duration-300 transform hover:scale-110 hover:rotate-3"
-            >
-              <img src={alfaLogo} alt="Alfamart" className="w-32 h-auto" />
-            </a>
+          {/* Logo Alfamart */}
+          <a
+            href="https://www.alfamart.co.id/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-transform duration-300 transform hover:scale-110 hover:rotate-3"
+          >
+            <img src={alfaLogo} alt="Alfamart" className="w-32 h-auto" />
+          </a>
 
-            {/* Logo CTI Group */}
-            <a
-              href="https://www.ctigroup.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="transition-transform duration-300 transform hover:scale-110 hover:rotate-3"
-            >
-              <img src={ctiLogo} alt="CTI Group" className="w-32 h-auto" />
-            </a>
+          {/* Logo CTI Group */}
+          <a
+            href="https://www.ctigroup.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-transform duration-300 transform hover:scale-110 hover:rotate-3"
+          >
+            <img src={ctiLogo} alt="CTI Group" className="w-32 h-auto" />
+          </a>
 
-            {/* Logo Pura */}
-            <a
-              href="https://www.pura.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="transition-transform duration-300 transform hover:scale-110 hover:rotate-3"
-            >
-              <img src={puraLogo} alt="Pura" className="w-32 h-auto" />
-            </a>
+          {/* Logo Pura */}
+          <a
+            href="https://www.pura.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-transform duration-300 transform hover:scale-110 hover:rotate-3"
+          >
+            <img src={puraLogo} alt="Pura" className="w-32 h-auto" />
+          </a>
 
-            {/* Logo Sinarmas */}
-            <a
-              href="https://www.sinarmas.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="transition-transform duration-300 transform hover:scale-110 hover:rotate-3"
-            >
-              <img src={sinarmasLogo} alt="Sinarmas" className="w-32 h-auto" />
-            </a>
+          {/* Logo Sinarmas */}
+          <a
+            href="https://www.sinarmas.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-transform duration-300 transform hover:scale-110 hover:rotate-3"
+          >
+            <img src={sinarmasLogo} alt="Sinarmas" className="w-32 h-auto" />
+          </a>
 
-            {/* Logo BCA */}
-            <a
-              href="https://www.bca.co.id/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="transition-transform duration-300 transform hover:scale-110 hover:rotate-3"
-            >
-              <img src={bcaLogo} alt="BCA" className="w-32 h-auto" />
-            </a>
+          {/* Logo BCA */}
+          <a
+            href="https://www.bca.co.id/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-transform duration-300 transform hover:scale-110 hover:rotate-3"
+          >
+            <img src={bcaLogo} alt="BCA" className="w-32 h-auto" />
+          </a>
           {/* </div> */}
         </div>
       </div>
@@ -263,7 +231,7 @@ function Index() {
       </div>
 
       {/*Bagian Layanan Kampus*/}
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-8 py-8">
         {/* Heading Layanan Kampus */}
         <div className="text-center">
           <h2 className="text-3xl font-semibold">Layanan Kampus</h2>
@@ -272,7 +240,7 @@ function Index() {
         {/* Gambar Layanan Kampus */}
         <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-1">
           {/* Gambar 1 */}
-          <div className="w-full p-4">
+          <div className="w-full py-4">
             <a
               href="https://siasat.uksw.edu"
               target="_blank"
@@ -287,7 +255,7 @@ function Index() {
           </div>
 
           {/* Gambar 2 */}
-          <div className="w-full p-4">
+          <div className="w-full py-4">
             <a
               href="http://online.fti.uksw.edu"
               target="_blank"
@@ -302,7 +270,7 @@ function Index() {
           </div>
 
           {/* Gambar 3 */}
-          <div className="w-full p-4">
+          <div className="w-full py-4">
             <a
               href="https://ejournal.uksw.edu/itexplore"
               target="_blank"
@@ -317,7 +285,7 @@ function Index() {
           </div>
 
           {/* Gambar 4 */}
-          <div className="w-full p-4">
+          <div className="w-full py-4">
             <a
               href="https://library.uksw.edu"
               target="_blank"
@@ -332,7 +300,7 @@ function Index() {
           </div>
 
           {/* Gambar 5 */}
-          <div className="w-full p-4">
+          <div className="w-full py-4">
             <a
               href="https://ejournal.uksw.edu/aiti"
               target="_blank"
