9b19936cefe5f4dce5d252fdfae07a6911897c94
Chr1ztt <chilllikehuman@gmail.com>
2025-05-06 16:00:08 +0700

    chore: modify theme button

--- frontend/src/features/shared/components/themeToggleButton.tsx
+++ frontend/src/features/shared/components/themeToggleButton.tsx
@@ -1,4 +1,10 @@
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { useTheme } from "@/hooks/useTheme";
 import { cn } from "@/lib/utils";
 import { MoonIcon, SunIcon, SunMoonIcon } from "lucide-react";
@@ -10,22 +16,52 @@ const ThemeToggleButton = React.forwardRef<
 >(({ className, ...props }, ref) => {
   const { theme, setTheme } = useTheme();
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      className={cn("px-2", className)}
-      ref={ref}
-      {...props}
-      onClick={() => setTheme(theme === "light" ? "dark" : theme === "dark" ? "system" : "light")}
-    >
-      {theme === "dark" ? (
-        <MoonIcon className="h-4 w-4" />
-      ) : theme == "light" ? (
-        <SunIcon className="h-4 w-4" />
-      ) : (
-        <SunMoonIcon className="h-4 w-4" />
-      )}
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className={cn("px-2", className)}
+          ref={ref}
+          {...props}
+          onClick={() =>
+            setTheme(
+              theme === "light"
+                ? "dark"
+                : theme === "dark"
+                  ? "system"
+                  : "light",
+            )
+          }
+        >
+          {theme === "dark" ? (
+            <MoonIcon className="h-4 w-4" />
+          ) : theme == "light" ? (
+            <SunIcon className="h-4 w-4" />
+          ) : (
+            <SunMoonIcon className="h-4 w-4" />
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        className="min-w-20"
+        onCloseAutoFocus={(e) => e.preventDefault()}
+      >
+        <DropdownMenuItem onClick={() => setTheme("light")} className="justify-end">
+          <span>Light</span>
+          <SunIcon className="h-4 w-4" />
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")} className="justify-end">
+          <span>Dark</span>
+          <MoonIcon className="h-4 w-4" />
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")} className="justify-end">
+          <span>System</span>
+          <SunMoonIcon className="h-4 w-4" />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 });
 
