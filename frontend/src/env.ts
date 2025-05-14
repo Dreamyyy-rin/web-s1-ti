2dd81823063cf465495076b23f4e23042bca2288
Chr1ztt <chilllikehuman@gmail.com>
2025-05-14 17:43:56 +0700

    chore: remove unused env variables

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -9,11 +9,8 @@ export function checkEnv() {
 
 export const ENV = {
   APP: {
-    TOKEN_ENCRYPTION_KEY: import.meta.env.VITE_APP_TOKEN_ENCRYPTION_KEY,
-    USER_ENCRYPTION_KEY: import.meta.env.VITE_APP_USER_ENCRYPTION_KEY,
     BACKEND_URL: import.meta.env.VITE_APP_BACKEND_URL,
     FRONTEND_URL: import.meta.env.VITE_APP_FRONTEND_URL,
-    BACKEND_STORAGE_URL: import.meta.env.VITE_APP_BACKEND_STORAGE_URL,
     MODE: import.meta.env.VITE_APP_MODE,
     IS_REGISTER_ENABLED: String(import.meta.env.VITE_APP_IS_REGISTER_ENABLED).toLowerCase() === "true",
   },
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,10 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_APP_TOKEN_ENCRYPTION_KEY: string;
-  readonly VITE_APP_USER_ENCRYPTION_KEY: string;
   readonly VITE_APP_BACKEND_URL: string;
   readonly VITE_APP_FRONTEND_URL: string;
-  readonly VITE_APP_BACKEND_STORAGE_URL: string;
   readonly VITE_APP_IS_REGISTER_ENABLED: string;
   readonly VITE_APP_MODE: "development" | "production" | "staging";
   readonly VITE_GOOGLE_CLIENT_ID: string;
